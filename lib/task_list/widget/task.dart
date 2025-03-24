723b0e49ae47700db175593134369b14c1e38dd2
bokrosf <bokros.feri@hotmail.com>
2025-03-24 13:50:06 +0100

    onCompletionToggled callback added to Task widget.

--- lib/task_list/widget/task.dart
+++ lib/task_list/widget/task.dart
@@ -3,8 +3,14 @@ import 'package:todo/task_list/view_model/task_view_model.dart';
 
 class Task extends StatelessWidget {
   final TaskViewModel viewModel;
+  final void Function(int) _onCompletionToggled;
 
-  const Task({super.key, required this.viewModel});
+  const Task({
+    super.key,
+    required this.viewModel,
+    required void Function(int) onCompletionToggled
+  })
+    : _onCompletionToggled = onCompletionToggled;
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +18,10 @@ class Task extends StatelessWidget {
       children: [
         Checkbox(
           value: viewModel.isCompleted,
-          onChanged: (value) {
-            print('Completion changed: $value');
-          },
+          onChanged: (value) => _onCompletionToggled.call(viewModel.id),
         ),
         Text(viewModel.text),
-      ],);
+      ],
+    );
   }
 }

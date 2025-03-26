7fa01930f5639a1e12899d08a3a4ffa9b43fb0e1
bokrosf <bokros.feri@hotmail.com>
2025-03-26 12:07:25 +0100

    Color property added to Task widget.

--- lib/task_list/widget/task.dart
+++ lib/task_list/widget/task.dart
@@ -4,11 +4,13 @@ import 'package:todo/task_list/view_model/task_view_model.dart';
 class Task extends StatelessWidget {
   final TaskViewModel viewModel;
   final void Function(int) _onCompletionToggled;
+  final Color? color;
 
   const Task({
     super.key,
     required this.viewModel,
-    required void Function(int) onCompletionToggled
+    required void Function(int) onCompletionToggled,
+    this.color,
   })
     : _onCompletionToggled = onCompletionToggled;
 
@@ -18,7 +20,7 @@ class Task extends StatelessWidget {
       listenable: viewModel,
       builder: (_, _) {
         return Container(
-          color: Colors.white,
+          color: color,
           child: Row(
             children: [
               Checkbox(

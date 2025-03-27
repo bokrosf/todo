807a7fdcc7790a603fec7559876e5f43211590bb
bokrosf <bokros.feri@hotmail.com>
2025-03-27 15:34:18 +0100

    widgets folder removed to view.

--- lib/task_list/view/task_list_screen.dart
+++ lib/task_list/view/task_list_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo/task_list/view_model/task_list_view_model.dart';
-import 'package:todo/task_list/widget/task_list.dart';
+import 'package:todo/task_list/view/task_list.dart';
 
 class TaskListScreen extends StatefulWidget {
   final TaskListViewModel _viewModel;

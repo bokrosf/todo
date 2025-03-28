7b16fc67a7e3a32b9e70c0fa8f3218055dad0618
bokrosf <bokros.feri@hotmail.com>
2025-03-28 16:16:21 +0100

    Database classes can be generated.

--- lib/data/task.dart
+++ lib/data/task.dart
@@ -1,5 +1,7 @@
 import 'package:isar/isar.dart';
 
+part 'task.g.dart';
+
 @collection
 class Task {
   Id id = Isar.autoIncrement;
--- lib/data/task_list.dart
+++ lib/data/task_list.dart
@@ -1,5 +1,7 @@
 import 'package:isar/isar.dart';
 
+part 'task_list.g.dart';
+
 @collection
 class TaskList {
   Id id = Isar.autoIncrement;

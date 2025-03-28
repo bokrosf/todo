c7dc4bb4d1b126c9641ab73fb1e7b747b8851463
bokrosf <bokros.feri@hotmail.com>
2025-03-28 21:28:08 +0100

    Task constructor added.

--- lib/data/task.dart
+++ lib/data/task.dart
@@ -5,11 +5,19 @@ part 'task.g.dart';
 @collection
 class Task {
   Id id = Isar.autoIncrement;
-  int listId = 0;
-  String text = '';
+  int listId;
+  String text;
   int? parentId;
-  DateTime createdAt = DateTime.now();
+  DateTime createdAt;
   DateTime? plannedAt;
   DateTime? completedAt;
   String? note;
+
+  Task({
+    required this.listId,
+    required this.text,
+    this.parentId,
+    required this.createdAt,
+    this.note,
+  });
 }

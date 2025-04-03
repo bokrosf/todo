aa10cdbdee97352288f3a8ecb1f8904e527b36f5
bokrosf <bokros.feri@hotmail.com>
2025-04-04 00:10:59 +0200

    ProjectViewModel added.

---
+++ lib/feature/project_list/view_model/project_view_model.dart
@@ -0,0 +1,11 @@
+class ProjectViewModel {
+  final int id;
+  String name;
+  final bool builtin;
+
+  ProjectViewModel({
+    required this.id,
+    required this.name,
+    required this.builtin,
+  });
+}

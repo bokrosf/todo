6c085e07d382033c6badf767a0d689ebeea43ea5
bokrosf <bokros.feri@hotmail.com>
2025-04-04 15:58:44 +0200

    builtin, custom properties added to ProjectListViewModel.

--- lib/feature/project_list/view_model/project_list_view_model.dart
+++ lib/feature/project_list/view_model/project_list_view_model.dart
@@ -1,10 +1,23 @@
+import 'dart:collection';
+
 import 'package:todo/feature/project_list/model/project_list_model.dart';
+import 'package:todo/feature/project_list/view_model/project_view_model.dart';
 
 class ProjectListViewModel {
   final ProjectListModel _model;
+  List<ProjectViewModel> _projects = [];
 
   ProjectListViewModel({
     required ProjectListModel model,
   })
     : _model = model;
+
+  UnmodifiableListView get builtin {
+    return UnmodifiableListView(_projects.where((p) => p.builtin));
+  }
+
+  UnmodifiableListView get custom {
+    return UnmodifiableListView(_projects.where((p) => !p.builtin));
+  }
+
 }
